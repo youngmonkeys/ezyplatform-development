ad21d3d2e2364550da64ea754cda1773df13ad67
Dung Ta Van <itprono3@gmail.com>
2022-05-22 08:32:43 +0700

    update admin roles

--- ezyplatform-sdk/ezyplatform-common-sdk/src/main/java/org/youngmonkeys/ezyplatform/data/AdminRolesProxy.java
+++ ezyplatform-sdk/ezyplatform-common-sdk/src/main/java/org/youngmonkeys/ezyplatform/data/AdminRolesProxy.java
@@ -21,10 +21,7 @@ import com.tvd12.ezyhttp.server.core.manager.FeatureURIManager;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static com.tvd12.ezyfox.io.EzyCollections.containsAny;
 
@@ -34,21 +31,33 @@ public class AdminRolesProxy {
     private boolean supperAdmin;
     @Getter
     private boolean specialAdmin;
-    private Set<String> accessibleUris;
     private FeatureURIManager featureUriManager;
+    private Map<String, Set<HttpMethod>> accessibleMethodsByUri;
 
-    public boolean isAccessible(String url) {
+    public boolean isAccessible(String uri) {
+        return isAccessible(uri, HttpMethod.GET);
+    }
+
+    public boolean isAccessible(String uri, String method) {
+        return isAccessible(uri, HttpMethod.valueOf(method));
+    }
+
+    public boolean isAccessible(String uri, HttpMethod method) {
         if (specialAdmin) {
             return true;
         }
         String feature = featureUriManager.getFeatureByURI(
-            HttpMethod.GET,
-            url
+            method,
+            uri
         );
         if (feature == null) {
             return true;
         }
-        return accessibleUris.contains(url);
+        Set<HttpMethod> methods = accessibleMethodsByUri.getOrDefault(
+            uri,
+            Collections.emptySet()
+        );
+        return methods.contains(method);
     }
 
     public boolean isAccessibleAny(List<String> urls) {
@@ -67,23 +76,28 @@ public class AdminRolesProxy {
         FeatureURIManager featureUriManager,
         Map<Long, Map<String, Map<String, Set<HttpMethod>>>> methodsUriMapByFeatureByRoleId
     ) {
-        Set<String> accessibleUris = new HashSet<>();
+        Map<String, Set<HttpMethod>> accessibleMethodsByUri = new HashMap<>();
         for (Long roleId : roleIds) {
             Map<String, Map<String, Set<HttpMethod>>> methodsUriMapByFeature =
                 methodsUriMapByFeatureByRoleId.get(roleId);
             if (methodsUriMapByFeature != null) {
                 for (Map<String, Set<HttpMethod>> methodsUriMap : methodsUriMapByFeature.values()) {
                     for (Map.Entry<String, Set<HttpMethod>> entry : methodsUriMap.entrySet()) {
-                        if (entry.getValue().contains(HttpMethod.GET)) {
-                            accessibleUris.add(entry.getKey());
-                        }
+                        accessibleMethodsByUri.compute(entry.getKey(), (k, v) -> {
+                            if (v == null) {
+                                return entry.getValue();
+                            }
+                            Set<HttpMethod> methods = new HashSet<>(v);
+                            methods.addAll(entry.getValue());
+                            return methods;
+                        });
                     }
                 }
             }
         }
         return AdminRolesProxy.builder()
-            .accessibleUris(accessibleUris)
             .featureUriManager(featureUriManager)
+            .accessibleMethodsByUri(accessibleMethodsByUri)
             .specialAdmin(containsAny(roleIds, specialRoleIds))
             .supperAdmin(roleIds.contains(superAdminRoleId))
             .build();
