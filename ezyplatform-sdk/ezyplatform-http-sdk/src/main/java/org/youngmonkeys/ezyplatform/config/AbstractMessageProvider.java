150ce7034b767ba6a97ffe160497973cd32b7997
Dung Ta Van <itprono3@gmail.com>
2023-01-20 23:34:27 +0700

    update ezyhttp

---
+++ ezyplatform-sdk/ezyplatform-http-sdk/src/main/java/org/youngmonkeys/ezyplatform/config/AbstractMessageProvider.java
@@ -0,0 +1,84 @@
+package org.youngmonkeys.ezyplatform.config;
+
+import com.tvd12.ezyhttp.server.core.view.MessageProvider;
+import com.tvd12.ezyhttp.server.core.view.MessageReader;
+import lombok.AllArgsConstructor;
+import org.youngmonkeys.ezyplatform.entity.ModuleType;
+import org.youngmonkeys.ezyplatform.entity.TargetType;
+import org.youngmonkeys.ezyplatform.manager.FileSystemManager;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+@AllArgsConstructor
+public abstract class AbstractMessageProvider implements MessageProvider {
+
+    private final FileSystemManager fileSystemManager;
+    private static final String MESSAGES_FOLDER = "resources/messages";
+
+    @Override
+    public Map<String, Properties> provide() {
+        String ezyplatformHomePath = fileSystemManager.getEzyHomePathString();
+        Map<String, Properties> answer = new HashMap<>();
+        TargetType targetType = getInclusiveTargetType();
+        readAndAppendMessages(
+            answer,
+            Paths.get(
+                ezyplatformHomePath,
+                targetType.getName(),
+                MESSAGES_FOLDER
+            )
+        );
+        ModuleType[] moduleTypes = getInclusiveModuleTypes();
+        Map<ModuleType, List<String>> modulesMap = getInclusiveModulesMapByModuleTypes(
+            moduleTypes
+        );
+        for (ModuleType moduleType : moduleTypes) {
+            List<String> modules = modulesMap.get(moduleType);
+            for (String module : modules) {
+                readAndAppendMessages(
+                    answer,
+                    Paths.get(
+                        ezyplatformHomePath,
+                        moduleType.getTargetFolder(),
+                        module,
+                        MESSAGES_FOLDER
+                    )
+                );
+            }
+        }
+        return answer;
+    }
+
+    private void readAndAppendMessages(
+        Map<String, Properties> messages,
+        Path messageFolder
+    ) {
+        if (!Files.exists(messageFolder)) {
+            return;
+        }
+        MessageReader messageReader = MessageReader.getDefault();
+        Map<String, Properties> map = messageReader.read(
+            messageFolder.toString()
+        );
+        for (Map.Entry<String, Properties> e : map.entrySet()) {
+            String lang = e.getKey();
+            messages
+                .computeIfAbsent(lang, k -> new Properties())
+                .putAll(e.getValue());
+        }
+    }
+
+    protected abstract TargetType getInclusiveTargetType();
+
+    protected abstract ModuleType[] getInclusiveModuleTypes();
+
+    protected abstract Map<ModuleType, List<String>> getInclusiveModulesMapByModuleTypes(
+        ModuleType[] moduleTypes
+    );
+}
