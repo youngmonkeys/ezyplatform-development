543c040fe20fc10a9c1bf6d9f49a2c881d6cc210
tvd12 <ta.van.dung@youngmonkeys.org>
2026-06-21 15:07:49 +0700

    update ViewMetadata

--- ezyplatform-sdk/ezyplatform-common-sdk/src/main/java/org/youngmonkeys/ezyplatform/service/DefaultUserMetaService.java
+++ ezyplatform-sdk/ezyplatform-common-sdk/src/main/java/org/youngmonkeys/ezyplatform/service/DefaultUserMetaService.java
@@ -38,6 +38,7 @@ import java.util.stream.Collectors;
 import static com.tvd12.ezyfox.io.EzyLists.newArrayList;
 import static com.tvd12.ezyfox.io.EzySets.newHashSet;
 import static org.youngmonkeys.ezyplatform.constant.CommonConstants.NULL_STRING;
+import static org.youngmonkeys.ezyplatform.constant.CommonConstants.ZERO_LONG;
 import static org.youngmonkeys.ezyplatform.util.Strings.toBigIntegerOrZero;
 
 @AllArgsConstructor
@@ -253,7 +254,7 @@ public class DefaultUserMetaService implements UserMetaService {
             metaValue
         )
             .map(UserMeta::getUserId)
-            .orElse(0L);
+            .orElse(ZERO_LONG);
     }
 
     @Override
